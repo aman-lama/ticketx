99e0499d429c919528022ad125060adbecf23611
Aman Lama <lama.aman1998@gmail.com>
2024-02-05 15:56:06 +0545

    Mysql setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
+  let result = [1,2,3]
   return (
-   <div>Hello World</div>
+   <div>
+    {
+      result.map(v => v + 1).reduce((prev, curr) => prev + curr)
+    }
+   </div>
   )
 }
