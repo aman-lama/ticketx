e4bc9734178fbaa3b7fb804bfd6c4b0525b1b6b6
Aman Lama <lama.aman1998@gmail.com>
2024-02-05 16:21:16 +0545

    New Ticket Page

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const TicketsPage = () => {
   return (
-    <Button>TicketsPage</Button>
+    <Button>
+        <Link href={'/tickets/new'}>
+            New Issue
+        </Link>
+    </Button>
   )
 }
 
