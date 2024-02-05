5db2a4a7663a5072f5b22aa6af8ed211882e9a8a
Aman Lama <lama.aman1998@gmail.com>
2024-02-05 12:30:34 +0545

    Navbar Active section commit

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { FaBugs } from 'react-icons/fa6'
+import classnames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname();
     const links = [
         {label: 'Dashboard', href: '/'},
         {label: 'Tickets', href: '/tickets'},
@@ -12,7 +17,7 @@ const NavBar = () => {
         <Link href="/" className='flex gap-2 items-center'><FaBugs className='h-8 w-8 text-slate-800' /><span className='font-bold text-3xl bg-gradient-to-r from-slate-600 to-orange-600 text-transparent bg-clip-text hover:cursor-pointer'>Ticket-X</span></Link>
         <ul className='flex space-x-6'>
             {
-                links.map(link => <Link key={link.href} href={link.href} className='text-xl text-slate-700 hover:text-orange-500 transition-colors'>{link.label}</Link>)
+                links.map(link => <Link key={link.href} href={link.href} className={classnames({'text-slate-700 font-semibold': link.href === currentPath, 'text-slate-500 font-semibold': link.href !==currentPath, 'text-xl hover:text-orange-500 transition-colors': true})}>{link.label}</Link>)
             }
         </ul>
     </nav>
