cbe97296a0c6b728240c642ecc584d4b8afe5a63
Aman Lama <lama.aman1998@gmail.com>
2024-02-05 16:09:28 +0545

    Create Ticket Api

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
-  let result = [1,2,3]
   return (
    <div>
-    {
-      result.map(v => v + 1).reduce((prev, curr) => prev + curr)
-    }
+   Dashboard
    </div>
   )
 }
+
